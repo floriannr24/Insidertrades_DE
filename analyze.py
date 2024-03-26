bbf7b7afd34db6adf06fa7e1433820df2727b871
Florian Niedermeier <florian-niedermeier@gmx.de>
2024-03-26 20:56:43 +0100

    added various key measures

--- analyze.py
+++ analyze.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import statsmodels.formula.api as smf
 
-df = pd.read_excel("C:/Users/FSX-P/Insider_DE-df.xlsx")
+df = pd.read_excel("C:/Users/FSX-P/InsiderDE_2023-2024_Result.xlsx")
 
 df_new = df
 df_new["isVorstand"] = df["Position / Status"].apply(lambda x: 1 if x == "Vorstand" else 0)
@@ -16,34 +16,36 @@ df_new["%5"] = df_new["%5"].apply(lambda x: x*100)
 df_new["%10"] = df_new["%10"].apply(lambda x: x*100)
 df_new["%20"] = df_new["%20"].apply(lambda x: x*100)
 df_new["%30"] = df_new["%30"].apply(lambda x: x*100)
-df_new = df_new[df_new["Code"] != "DB1.DE"]
+# df_new = df_new[df_new["Code"] != "DB1.DE"]
 
-df_new["over500k"] = df_new["Aggregiertes Volumen"].apply(lambda x: 1 if x >= 500000 else 0)
-df_new["avg"] = df_new[["%5", "%10", "%20", "%30"]].mean(axis=1)
+# df_new["over500k"] = df_new["Aggregiertes Volumen"].apply(lambda x: 1 if x >= 500000 else 0)
+# df_new["avg"] = df_new[["%5", "%10", "%20", "%30"]].mean(axis=1)
 
 # counts = df_new["Code"].value_counts()
 # df_new = df_new[df_new["Code"].isin(counts[counts > 2].index)]
 # df_new = df[df["Aggregiertes Volumen"] > 100000]
 
-df_new = df_new.dropna()
-pd.set_option('display.max_columns', None)
 
-# df_to_std = df_new.copy()[["Aggregiertes Volumen", "%5", "%10", "%20", "%30"]]
+
+# df_new = df_new.dropna()
+# pd.set_option('display.max_columns', None)
 #
-# # scaler = StandardScaler()
-# # scaler.fit(df_to_std)
-# # df_std_new = scaler.transform(df_to_std)
-# # df_std = pd.DataFrame(df_std_new)
-
+# # df_to_std = df_new.copy()[["Aggregiertes Volumen", "%5", "%10", "%20", "%30"]]
+# #
+# # # scaler = StandardScaler()
+# # # scaler.fit(df_to_std)
+# # # df_std_new = scaler.transform(df_to_std)
+# # # df_std = pd.DataFrame(df_std_new)
+#
 # model = LinearRegression()
 # model.fit(df_new[["isVorstand", "Aggregiertes Volumen"]], df_new["avg"])
 #
 # print(model.score(df_new[["isVorstand", "Aggregiertes Volumen"]], df_new["avg"]))
-
-x = "Aggregiertes Volumen"
-y = "avg"
-
-sns.scatterplot(x=x, y=y, data=df_new)
-sns.regplot(x=x, y=y, data=df_new)
-
-plt.show()
+#
+# x = "Aggregiertes Volumen"
+# y = "avg"
+#
+# sns.scatterplot(x=x, y=y, data=df_new)
+# sns.regplot(x=x, y=y, data=df_new)
+#
+# plt.show()
--- file.py
+++ file.py
@@ -2,54 +2,159 @@ import datetime
 from datetime import timedelta, date
 import yfinance as yf
 import pandas as pd
+import pytz
 
-df = pd.read_excel("C:/Users/FSX-P/Insider_DE.xlsx")
+# df = pd.read_excel("C:/Users/FSX-P/InsiderDE_2023-2024.xlsx")
+#
+# holidays = ["2023-04-10", "2023-05-01", "2023-12-25", "2023-12-26", "2024-01-01", "2023-04-07"]
+# holidays_dates = [datetime.datetime.strptime(x, "%Y-%m-%d") for x in holidays]
+#
+# def dateIsHoliday(date):
+#     return date in holidays_dates
+#
+#
+# def dateAfterDays(date, days):
+#     newDate = date + timedelta(days=days)
+#
+#     if newDate.isoweekday() == 6:
+#         newDate = newDate + timedelta(days=2)
+#
+#     if newDate.isoweekday() == 7:
+#         newDate = newDate + timedelta(days=1)
+#
+#     if dateIsHoliday(newDate):
+#         newDate = dateAfterDays(newDate, 1)
+#
+#     return newDate
+#
+#
+# df = df.dropna()
+#
+# df["Aggregiertes Volumen"] = df["Aggregiertes Volumen"].str[:-4]
+# df["Aggregiertes Volumen"] = df["Aggregiertes Volumen"].str.replace(".", "").replace(",", ".")
+# df["Aggregiertes Volumen"] = (df["Aggregiertes Volumen"].str.replace(",", ".")).astype(float)
+#
+# df["Durchschnittspreis"] = df["Durchschnittspreis"].str[:-4]
+# df["Durchschnittspreis"] = df["Durchschnittspreis"].str.replace(".", "").replace(",", ".")
+# df["Durchschnittspreis"] = (df["Durchschnittspreis"].str.replace(",", ".")).astype(float)
+#
+# for index, row in df.iterrows():
+#
+#     dateOfTrade = row["Datum des Geschäfts"].to_pydatetime()
+#     tradePrice = row["Durchschnittspreis"]
+#     upperthreshold = round(tradePrice * 1.02, 2)
+#     lowerthreshold = round(tradePrice * 0.92, 2)
+#
+#     date5 = dateAfterDays(dateOfTrade, 5)
+#     date10 = dateAfterDays(dateOfTrade, 10)
+#     date20 = dateAfterDays(dateOfTrade, 20)
+#     date30 = dateAfterDays(dateOfTrade, 30)
+#
+#     if date5.date() > date.today() or date10.date() > date.today() or date20.date() > date.today() or date30.date() > date.today():
+#         continue
+#
+#     ticker = yf.Ticker(row["Code"])
+#
+#     history = ticker.history(start=date5, end=date5 + timedelta(days=1))
+#     price5 = None
+#     for i, day in history.iterrows():
+#         price5 = round(day["Close"], 2)
+#
+#     history = ticker.history(start=date10, end=date10 + timedelta(days=1))
+#     price10 = None
+#     for i, day in history.iterrows():
+#         price10 = round(day["Close"], 2)
+#
+#     history = ticker.history(start=date20, end=date20 + timedelta(days=1))
+#     price20 = None
+#     for i, day in history.iterrows():
+#         price20 = round(day["Close"], 2)
+#
+#     history = ticker.history(start=date30, end=date30 + timedelta(days=1))
+#     price30 = None
+#     for i, day in history.iterrows():
+#         price30 = round(day["Close"], 2)
+#
+#     overThreshold = 0
+#     underThreshold = 0
+#     dayOfUpperThreshold = None
+#     dayOfLowerThreshold = None
+#
+#     history = ticker.history(start=dateOfTrade + timedelta(days=1), end=date30 + timedelta(days=1))
+#     for i, day in history.iterrows():
+#
+#         if day["High"] > upperthreshold:
+#             overThreshold += 1
+#             if overThreshold == 1:
+#                 dayOfUpperThreshold = i
+#                 df.at[index, "daysAfterTradeUpperTHR"] = (dayOfUpperThreshold.replace(tzinfo = None) - dateOfTrade).days
+#
+#         if day["Low"] < lowerthreshold:
+#                 underThreshold += 1
+#                 if underThreshold == 1:
+#                     dayOfLowerThreshold = i
+#                     df.at[index, "daysAfterTradeLowerTHR"] = (dayOfLowerThreshold.replace(tzinfo = None) - dateOfTrade).days
+#
+#     tradingDays = len(history)
+#
+#     lower = False
+#
+#     if dayOfUpperThreshold and dayOfLowerThreshold:
+#         if dayOfUpperThreshold < dayOfLowerThreshold:
+#             df.at[index, "ThresholdReachedFirst"] = "upper"
+#         if dayOfUpperThreshold > dayOfLowerThreshold:
+#             df.at[index, "ThresholdReachedFirst"] = "lower"
+#             lower = True
+#
+#     if lower or (underThreshold > 0 and overThreshold == 0):
+#         df.at[index, "KO"] = "KO"
+#
+#     df.at[index, "overThresholdIn30days"] = overThreshold
+#     df.at[index, "underThresholdIn30days"] = underThreshold
+#     df.at[index, "tradingDays"] = tradingDays
+#
+#     if price5:
+#         df.at[index, "%5"] = price5 / row["Durchschnittspreis"] - 1
+#     if price10:
+#         df.at[index, "%10"] = price10 / row["Durchschnittspreis"] - 1
+#     if price20:
+#         df.at[index, "%20"] = price20 / row["Durchschnittspreis"] - 1
+#     if price30:
+#         df.at[index, "%30"] = price30 / row["Durchschnittspreis"] - 1
+#
+#     print(index)
+#
+# df.to_excel("C:/Users/FSX-P/InsiderDE_2023-2024_Result.xlsx", index=True, index_label="Index")
+
+df = pd.read_excel("C:/Users/FSX-P/InsiderDE_2023-2024_Result.xlsx")
+
+df = df.sort_values(by="Datum des Geschäfts", ascending=False)
+
+df["clump"] = None
+clumps = []
+timeframe = 4
+tradesInTimeframe = 3
 
 for index, row in df.iterrows():
+    code = row["Code"]
 
-    date5 = row["+5 Tage"].to_pydatetime()
-    date10 = row["+10 Tage"].to_pydatetime()
-    date20 = row["+20 Tage"].to_pydatetime()
-    date30 = row["+30 Tage"].to_pydatetime()
+    tradeDateOuter = row["Datum des Geschäfts"].to_pydatetime()
+    dateEnd = tradeDateOuter - timedelta(days=timeframe)
+    clumpIndex = 0
+    clumpToCode = []
 
-    if date5.date() > date.today() or date10.date() > date.today() or date20.date() > date.today() or date30.date() > date.today():
-        continue
+    for sindex, srow in df.iterrows():
 
-    ticker = yf.Ticker(row["Code"])
+        if srow["Code"] == code and not srow["clump"]:
+            tradeDateInner = srow["Datum des Geschäfts"].to_pydatetime()
+            if tradeDateInner < dateEnd or tradeDateInner > tradeDateOuter:
+                break
+            clumpToCode.append(sindex)
 
-    history = ticker.history(start=date5, end=date5 + timedelta(days=1))
-    price5 = None
-    for i, day in history.iterrows():
-        price5 = round(day["Close"], 2)
-    df.at[index, "Kurs5"] = price5
+    if len(clumpToCode) >= tradesInTimeframe and not df.loc[index, "clump"]:
+        clumpName = code + "_" + str(clumpIndex)
 
-    history = ticker.history(start=date10, end=date10 + timedelta(days=1))
-    price10 = None
-    for i, day in history.iterrows():
-        price10 = round(day["Close"], 2)
-    df.at[index, "Kurs10"] = price10
+        for clumpPart in clumpToCode:
+            df.loc[clumpPart, "clump"] = clumpName
 
-    history = ticker.history(start=date20, end=date20 + timedelta(days=1))
-    price20 = None
-    for i, day in history.iterrows():
-        price20 = round(day["Close"], 2)
-    df.at[index, "Kurs20"] = price20
-
-    history = ticker.history(start=date30, end=date30 + timedelta(days=1))
-    price30 = None
-    for i, day in history.iterrows():
-        price30 = round(day["Close"], 2)
-    df.at[index, "Kurs30"] = price30
-
-    if price5:
-        df.at[index, "%5"] = price5/row["Durchschnittspreis"]-1
-    if price10:
-        df.at[index, "%10"] = price10/row["Durchschnittspreis"]-1
-    if price20:
-        df.at[index, "%20"] = price20/row["Durchschnittspreis"]-1
-    if price30:
-        df.at[index, "%30"] = price30/row["Durchschnittspreis"]-1
-
-    print(index)
-
-df.to_excel("C:/Users/FSX-P/Insider_DE-df1.xlsx", index=False)
+df.to_excel("C:/Users/FSX-P/InsiderDE_2023-2024_Result.xlsx", index=False)
