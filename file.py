183e638b9c5309b79c0d45f6fa4520f0acffc8bb
Florian Niedermeier <florian-niedermeier@gmx.de>
2025-09-04 23:31:29 +0200

    update

--- file.py
+++ file.py
@@ -1,8 +1,12 @@
 import datetime
 from datetime import timedelta, date
+
+import numpy as np
 import yfinance as yf
 import pandas as pd
 import copy
+
+from yfinance.exceptions import YFException
 
 holidays = ["2023-04-10", "2023-05-01", "2023-12-25", "2023-12-26", "2024-01-01", "2023-04-07"]
 holidays_dates = [datetime.datetime.strptime(x, "%Y-%m-%d") for x in holidays]
@@ -25,122 +29,142 @@ def dateAfterDays(date, days):
     return newDate
 
 
-def fetchAndSaveData():
+def fetchData(df):
 
-    df_fetchedData = pd.read_excel("C:/Users/FSX-P/bafin/Gesamt.xlsx")
-    df_base = copy.deepcopy(df_fetchedData)
+    historyCached = {
+        "code": None,
+        "lastDate": None,
+        "history": None
+    }
 
-    df_fetchedData = df_fetchedData.dropna(subset=["Durchschnittspreis"])
+    for i, row in df.iterrows():
 
-    df_fetchedData["Aggregiertes Volumen"] = df_fetchedData["Aggregiertes Volumen"].str[:-4]
-    df_fetchedData["Aggregiertes Volumen"] = df_fetchedData["Aggregiertes Volumen"].str.replace(".", "").replace(",", ".")
-    df_fetchedData["Aggregiertes Volumen"] = (df_fetchedData["Aggregiertes Volumen"].str.replace(",", ".")).astype(float)
+        code = row["Code"]
 
-    df_fetchedData["Durchschnittspreis"] = df_fetchedData["Durchschnittspreis"].str[:-4]
-    df_fetchedData["Durchschnittspreis"] = df_fetchedData["Durchschnittspreis"].str.replace(".", "").replace(",", ".")
-    df_fetchedData["Durchschnittspreis"] = (df_fetchedData["Durchschnittspreis"].str.replace(",", ".")).astype(float)
-
-    for index, row in df_fetchedData.iterrows():
+        if pd.isnull(code):
+            continue
 
         dateOfTrade = row["Datum des Geschäfts"].to_pydatetime()
-        tradePrice = row["Durchschnittspreis"]
-        upperthreshold = round(tradePrice * 1.02, 2)
-        lowerthreshold = round(tradePrice * 0.92, 2)
 
         date5 = dateAfterDays(dateOfTrade, 5)
         date10 = dateAfterDays(dateOfTrade, 10)
         date20 = dateAfterDays(dateOfTrade, 20)
         date30 = dateAfterDays(dateOfTrade, 30)
+        date60 = dateAfterDays(dateOfTrade, 60)
 
-        if not pd.isnull(df_fetchedData.loc[index, "DataFetched"]):
+        if not pd.isnull(df.loc[i, "DataFetched"]):
             continue
 
         if date5.date() > date.today() or date10.date() > date.today() or date20.date() > date.today() or date30.date() > date.today():
             continue
-    
-        ticker = yf.Ticker(row["Code"])
 
-        history = ticker.history(start=date5, end=date5 + timedelta(days=1))
+        if historyCached["code"] == code and historyCached["lastDate"] >= date30:
+            history = historyCached["history"]
+        else:
+            ticker = yf.Ticker(code)
+            history = ticker.history(start=dateOfTrade, end=date60 + timedelta(days=1))
+            history.index = pd.to_datetime(history.index).date
+
+            historyCached["code"] = code
+            historyCached["lastDate"] = date60
+            historyCached["history"] = history
+
         price5 = None
-        for i, day in history.iterrows():
-            price5 = round(day["Close"], 2)
-
-        history = ticker.history(start=date10, end=date10 + timedelta(days=1))
         price10 = None
-        for i, day in history.iterrows():
-            price10 = round(day["Close"], 2)
-
-        history = ticker.history(start=date20, end=date20 + timedelta(days=1))
         price20 = None
-        for i, day in history.iterrows():
-            price20 = round(day["Close"], 2)
-
-        history = ticker.history(start=date30, end=date30 + timedelta(days=1))
         price30 = None
-        for i, day in history.iterrows():
-            price30 = round(day["Close"], 2)
 
-        overThreshold = 0
-        underThreshold = 0
-        dayOfUpperThreshold = None
-        dayOfLowerThreshold = None
+        try:
+            price5 = history.loc[date5.date(), "Open"]
+            price10 = history.loc[date10.date(), "Open"]
+            price20 = history.loc[date20.date(), "Open"]
+            price30 = history.loc[date30.date(), "Open"]
+        except KeyError:
+            print(f"{code} Error while fetching price data")
+            print("---------")
+            continue
 
-        history = ticker.history(start=dateOfTrade + timedelta(days=1), end=date30 + timedelta(days=1))
-        for i, day in history.iterrows():
+        tradePrice = row["Durchschnittspreis"]
+        upperthreshold = round(tradePrice * 1.02, 2)
+        lowerthreshold = round(tradePrice * 0.92, 2)
 
-            if day["High"] > upperthreshold:
-                overThreshold += 1
-                if overThreshold == 1:
-                    dayOfUpperThreshold = i
-                    df_fetchedData.at[index, "daysAfterTradeUpperTHR"] = (dayOfUpperThreshold.replace(tzinfo = None) - dateOfTrade).days
-
-            if day["Low"] < lowerthreshold:
-                    underThreshold += 1
-                    if underThreshold == 1:
-                        dayOfLowerThreshold = i
-                        df_fetchedData.at[index, "daysAfterTradeLowerTHR"] = (dayOfLowerThreshold.replace(tzinfo = None) - dateOfTrade).days
-
-        tradingDays = len(history)
-
-        lower = False
-
-        if dayOfUpperThreshold and dayOfLowerThreshold:
-            if dayOfUpperThreshold < dayOfLowerThreshold:
-                df_fetchedData.at[index, "ThresholdReachedFirst"] = "upper"
-            if dayOfUpperThreshold > dayOfLowerThreshold:
-                df_fetchedData.at[index, "ThresholdReachedFirst"] = "lower"
-                lower = True
-
-        if lower or (underThreshold > 0 and overThreshold == 0):
-            df_fetchedData.at[index, "KO"] = "KO"
-
-        df_fetchedData.at[index, "overThresholdIn30days"] = overThreshold
-        df_fetchedData.at[index, "underThresholdIn30days"] = underThreshold
-        df_fetchedData.at[index, "tradingDays"] = tradingDays
+        # overThreshold = 0
+        # underThreshold = 0
+        # dayOfUpperThreshold = None
+        # dayOfLowerThreshold = None
+        #
+        # history = ticker.history(start=dateOfTrade + timedelta(days=1), end=date30 + timedelta(days=1))
+        # for i, day in history.iterrows():
+        #
+        #     if day["High"] > upperthreshold:
+        #         overThreshold += 1
+        #         if overThreshold == 1:
+        #             dayOfUpperThreshold = i
+        #             df_fetchedData.at[i, "daysAfterTradeUpperTHR"] = (
+        #                         dayOfUpperThreshold.replace(tzinfo=None) - dateOfTrade).days
+        #
+        #     if day["Low"] < lowerthreshold:
+        #         underThreshold += 1
+        #         if underThreshold == 1:
+        #             dayOfLowerThreshold = i
+        #             df_fetchedData.at[i, "daysAfterTradeLowerTHR"] = (
+        #                         dayOfLowerThreshold.replace(tzinfo=None) - dateOfTrade).days
+        #
+        # tradingDays = len(history)
+        #
+        # lower = False
+        #
+        # if dayOfUpperThreshold and dayOfLowerThreshold:
+        #     if dayOfUpperThreshold < dayOfLowerThreshold:
+        #         df_fetchedData.at[i, "ThresholdReachedFirst"] = "upper"
+        #     if dayOfUpperThreshold > dayOfLowerThreshold:
+        #         df_fetchedData.at[i, "ThresholdReachedFirst"] = "lower"
+        #         lower = True
+        #
+        # if lower or (underThreshold > 0 and overThreshold == 0):
+        #     df_fetchedData.at[i, "KO"] = "KO"
+        #
+        # df_fetchedData.at[i, "overThresholdIn30days"] = overThreshold
+        # df_fetchedData.at[i, "underThresholdIn30days"] = underThreshold
+        # df_fetchedData.at[i, "tradingDays"] = tradingDays
 
         if price5 and price10 and price20 and price30:
-            df_base.at[index, "DataFetched"] = "yes"
-            df_fetchedData.at[index, "DataFetched"] = "yes"
+            df.at[i, "DataFetched"] = "yes"
 
         if price5:
-            df_fetchedData.at[index, "%5"] = price5 / row["Durchschnittspreis"] - 1
+            df.at[i, "%5"] = price5 / row["Durchschnittspreis"] - 1
         if price10:
-            df_fetchedData.at[index, "%10"] = price10 / row["Durchschnittspreis"] - 1
+            df.at[i, "%10"] = price10 / row["Durchschnittspreis"] - 1
         if price20:
-            df_fetchedData.at[index, "%20"] = price20 / row["Durchschnittspreis"] - 1
+            df.at[i, "%20"] = price20 / row["Durchschnittspreis"] - 1
         if price30:
-            df_fetchedData.at[index, "%30"] = price30 / row["Durchschnittspreis"] - 1
+            df.at[i, "%30"] = price30 / row["Durchschnittspreis"] - 1
 
-        print(index, row["Code"])
+        print(row["Code"], row["Mitteilungsdatum"])
         print("---------")
 
-    df_fetchedData = df_fetchedData.dropna(subset=["DataFetched"])
-    df_oldData = pd.read_excel("C:/Users/FSX-P/bafin/InsiderDE_2023-2024_Result.xlsx")
+    return df
 
-    df_new = df_oldData + df_fetchedData
 
-    df_new.to_excel("C:/Users/FSX-P/bafin/InsiderDE_2023-2024_Result.xlsx", index=True, index_label="Index")
-    df_base.to_excel("C:/Users/FSX-P/bafin/Gesamt1.xlsx", index=True, index_label="Index")
+def fetchAndSaveData():
+
+    df = loadDataframe()
+    df_calculated = fetchData(df)
+    df_calculated.to_excel("C:/Users/FSX-P/bafin/InsiderDE_2023-2024_Result1.xlsx", index=False)
+
+
+def loadDataframe():
+    df = pd.read_excel("C:/Users/FSX-P/bafin/Gesamt.xlsx")
+    df = df.dropna(subset=["Durchschnittspreis"])
+    df["Aggregiertes Volumen"] = df["Aggregiertes Volumen"].str[:-4]
+    df["Aggregiertes Volumen"] = df["Aggregiertes Volumen"].str.replace(".", "").replace(",", ".")
+    df["Aggregiertes Volumen"] = (df["Aggregiertes Volumen"].str.replace(",", ".")).astype(float)
+    df["Durchschnittspreis"] = df["Durchschnittspreis"].str[:-4]
+    df["Durchschnittspreis"] = df["Durchschnittspreis"].str.replace(".", "").replace(",", ".")
+    df["Durchschnittspreis"] = (df["Durchschnittspreis"].str.replace(",", ".")).astype(float)
+    df = df.sort_values(["Code", "Mitteilungsdatum"], ascending=[True, True])
+    return df
+
 
 fetchAndSaveData()
 
